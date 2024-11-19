e6a127f5da517990dc32b37ae28fe57b0dff6251
annujames123455 <annujames2028@cs.sjcetpalai.ac.in>
2024-11-19 15:59:49 +0530

    Add files via upload

---
+++ annu3tuple.py
@@ -0,0 +1,34 @@
+'''
+author name:Annu James
+date:19/11/24
+'''
+fruits=("apple","banana","orange","date")
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+print(fruits[-1])
+print(len(fruits))
+even_numbers=(1,3, 5)
+odd_numbers=(2,4,6)
+numbers=even_numbers+odd_numbers
+print(numbers)
+repeat_tuple=("hello")*3
+print(repeat_tuple)
+print("banana" in fruits)
+colours=("red","green","yellow","orange","red")
+print(colours[-2:])
+person=('alice',25,"engineer")
+name,age,profession=person
+print(name)
+print(age)
+print(profession)
+stock=('bag',20,'750')
+thing,quantity,price=stock
+print(thing)
+print(quantity)
+print(price)
+inventory=[
+    ('laptop',5,30,000),
+    ('headphone',15,1000),
+    ('mouse',12,500),
+    
